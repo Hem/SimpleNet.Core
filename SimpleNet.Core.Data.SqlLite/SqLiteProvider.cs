b660d53c04eba530a0690b4b090dcdfb8c8a1679
Hem <HemTalreja@gmail.com>
2017-09-02 21:00:26 -0500

    Initial Checking -- .Net Core 2.0

---
+++ SimpleNet.Core.Data.SqlLite/SqLiteProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+
+namespace SimpleNet.Core.Data.SqLite
+{
+    /// <summary>
+    /// Sq lite provider.
+    /// </summary>
+    public class SqLiteProvider : ISimpleDatabaseProvider
+    {
+		readonly string _connectionString;
+
+		public SqLiteProvider(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public DbConnection GetConnection()
+		{
+            var connection = new SQLiteConnection(_connectionString);
+                connection.Open();
+            return connection;
+		}   
+
+        public DbCommand GetCommand()
+        {
+            return new SQLiteCommand();
+        }
+
+
+        public DbParameter GetParameter(string name, object value)
+        {
+            return new SQLiteParameter(name, value);
+        }
+
+        public DbParameter GetParameter(string name, object value, ParameterDirection direction)
+        {
+            return new SQLiteParameter(name, value) { Direction = direction };
+        }
+    }
+}
